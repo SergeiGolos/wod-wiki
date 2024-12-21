69df3d15606dad3729ef0abb956694eb009e51c9
Sergei Golos <sergei.golos@gmail.com>
2024-12-20 21:34:38 -0500

    getting the git action to run (#1)
    with feedback to the merge request

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export const helloWorld = () => {
   return 'Hello, World!';
 };
 
-
 console.log(helloWorld());
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
     },
   },
 });
