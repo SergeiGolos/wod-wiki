51a9f49ed554e00902d78aa46a9c5e22dc4cb40f
Sergei Golos <sergei.golos@gmail.com>
2025-04-03 15:02:44 -0400

    +semver: feature fixed issue with the way the system walks (#44)

--- src/core/runtime/actions/NextStatementAction.ts
+++ src/core/runtime/actions/NextStatementAction.ts
@@ -7,33 +7,19 @@ export class NextStatementAction implements IRuntimeAction {
 
   apply(runtime: ITimerRuntime):  RuntimeEvent[]   {
     const blocks = runtime.script.nodes;        
-    let current = runtime.current?.stack?.[runtime.current?.stack?.length - 1];
+    let current = runtime.current?.stack?.[0];
     
-    console.log("Next: Initial state", {
-      currentId: current?.id,
-      stackSize: runtime.current?.stack?.length
-    });
-
-    let blockId: number | undefined;    
-    const totalRounds = fragmentTo<RoundsFragment>(current!, 'rounds')?.count ?? 0;
-    const round = runtime.trace!.get(current?.id ?? -1) + 1;
+    let blockId: number | undefined;        
+    const round = runtime.trace!.get(current?.id ?? -1) + 1;        
     
-    console.log("Next: Block", {
-      id: current?.id,
-      round: `${round}/${totalRounds || '∞'}`,
-      children: current?.children.length
-    });
-        
-    if (totalRounds == 0 && round > totalRounds) {
-      console.log("Next: Rounds complete, moving to", current?.parent ?? current?.next);
+    if (current?.rounds == 0 && round > current?.rounds) {
       blockId = current?.parent ?? current?.next;
     } else {
-      console.log("Next: Rounds not complete, repeating", current?.id);      
+      blockId = current?.id;
     }
 
     const nextBlock = blocks.find(block => block.id == blockId);    
     const leaf = runtime.gotoBlock(nextBlock);
-    console.log("Next: Leaf", leaf);
     return leaf ? [{name:'start', timestamp: new Date()}] : [];
   }
 }
